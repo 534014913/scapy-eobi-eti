61311d4a04feef731c08e19102e87f5cbb8c6bc8
Yi Lai <534014913@qq.com>
2022-05-12 00:32:23 +0100

    Added Unit test and integrated test for packets generated

--- eobi_test.py
+++ eobi_test.py
@@ -2,22 +2,8 @@ from gen2 import *
 import subprocess
 import xml.etree.ElementTree as ET
 
-# if __name__ == "__Main__":
-# pkt = IP()/UDP(sport=65333, dport=65333)/PacketHeader(BodyLen=32) / \
-    # AddComplexInstrument(TemplateID=13400, NoLegs=2)
-# ls(pkt[AddComplexInstrument])
-
 XML_NAME = 'test_xml.xml'
 PROTOCOL_NAME = 'eurex.derivatives.eobi.t7.v10.0.'
-
-pkt = IP()/UDP(sport=65333, dport=65333)/PacketHeader()/AddComplexInstrument(NoLegs=2)
-
-pkt[PacketHeader].MessageHeader = MessageHeaderComp(BodyLen=32)
-pkt[AddComplexInstrument].MessageHeader = MessageHeaderComp(TemplateID=13400)
-pkt[AddComplexInstrument].InstrmtLegGrp = [InstrmtLegGrpComp(LegPrice=114000000), InstrmtLegGrpComp(LegPrice=514000000)]
-# ls(ppkt)
-# pkt.show2()
-# wrpcap('./trial.pcap', pkt, append=False)
 
 def print_if_error(sample_out):
     if sample_out is not None and  'Lua Error' in sample_out:
@@ -40,21 +26,6 @@ def xml_path(field):
 
 def get_show(xml, field):
     return xml.find(xml_path(field)).get('show')
-
-def add_compliex_instrument_test():
-
-    try:
-        result = subprocess.run(['tshark', '-V', '-r', 'trial.pcap'], check=True, capture_output=True, text=True)
-        print("stdout is:")
-        print(result.stdout)
-        print("")
-        print("stderr: ")
-        print(result.stderr)
-        print_if_error(result.stderr)
-    except subprocess.CalledProcessError as error:
-        print(error.stdout)
-        print(error.stderr)
-        raise error
 
 def packet_header_test():
     appl_seq_num = 20
@@ -143,6 +114,341 @@ def execution_summary_test():
         raise error
 
 
+def quote_request_test():
+    lst_qty = 25511
+    side = 2
+    sec_id = 78551
+
+    pkt = IP()/UDP(sport=65333, dport=65333)/PacketHeader()/QuoteRequest(SecurityID=sec_id, Side=side, LastQty=lst_qty)
+    pkt[PacketHeader].MessageHeader = MessageHeaderComp(TemplateID=13005)
+    pkt[QuoteRequest].MessageHeader = MessageHeaderComp(TemplateID=13503)
+
+    wrpcap('./test.pcap', pkt, append=False)
+    try:
+        result = subprocess.run(
+            ['tshark', '-V', '-r', 'test.pcap'], check=True, capture_output=True, text=True)
+        if print_if_error(result.stdout):
+            return
+        result = subprocess.run(
+            "tshark -T pdml -r test.pcap".split(" "), check=True, capture_output=True, text=True)
+        xml = ET.fromstring(result.stdout)
+        xml = xml.find(xml_path('message'))
+        # print(xml.attrib)
+
+        print('Testing for Quote Request Message Fields:')
+        print_if_not_match('SecurityID', get_show(
+            xml, 'securityid'), sec_id)
+        print_if_not_match('Side', get_show(
+            xml, 'side'), side)
+        print_if_not_match('LastQty', get_show(
+            xml, 'lastqty'), lst_qty / pow(10, 4))
+
+    except subprocess.CalledProcessError as error:
+        raise error
+
+def cross_request_test():
+    crx_type = 2
+    ipt_src = 1
+    pkt = IP()/UDP(sport=65333, dport=65333)/PacketHeader() /CrossRequest(CrossRequestType=crx_type, InputSource=ipt_src)
+    pkt[PacketHeader].MessageHeader = MessageHeaderComp(TemplateID=13005)
+    pkt[CrossRequest].MessageHeader = MessageHeaderComp(TemplateID=13502)
+
+    wrpcap('./test.pcap', pkt, append=False)
+    try:
+        result = subprocess.run(
+            ['tshark', '-V', '-r', 'test.pcap'], check=True, capture_output=True, text=True)
+        if print_if_error(result.stdout):
+            return
+        result = subprocess.run(
+            "tshark -T pdml -r test.pcap".split(" "), check=True, capture_output=True, text=True)
+        xml = ET.fromstring(result.stdout)
+        xml = xml.find(xml_path('message'))
+        # print(xml.attrib)
+
+        print('Testing for Cross Request Message Fields:')
+        print_if_not_match('CrossRequestType', get_show(
+            xml, 'crossrequesttype'), crx_type)
+        print_if_not_match('InputSource', get_show(
+            xml, 'inputsource'), ipt_src)
+
+    except subprocess.CalledProcessError as error:
+        raise error
+
+def trade_report_test():
+    mtch_typ = 5
+    trd_cond = 596
+
+    pkt = IP()/UDP(sport=65333, dport=65333)/PacketHeader() / \
+        TradeReport(MatchType=mtch_typ, TradeCondition=trd_cond)
+    pkt[PacketHeader].MessageHeader = MessageHeaderComp(TemplateID=13005)
+    pkt[TradeReport].MessageHeader = MessageHeaderComp(TemplateID=13201)
+
+    wrpcap('./test.pcap', pkt, append=False)
+    try:
+        result = subprocess.run(
+            ['tshark', '-V', '-r', 'test.pcap'], check=True, capture_output=True, text=True)
+        if print_if_error(result.stdout):
+            return
+        result = subprocess.run(
+            "tshark -T pdml -r test.pcap".split(" "), check=True, capture_output=True, text=True)
+        # print(result.stdout)
+        
+        xml = ET.fromstring(result.stdout)
+        xml = xml.find(xml_path('message'))
+        # print(xml.attrib)
+
+        print('Testing for Trade Report Message Fields:')
+        print_if_not_match('MatchType', get_show(
+            xml, 'matchtype'), mtch_typ)
+        print_if_not_match('TradeCondition', get_show(
+            xml, 'tradecondition'), trd_cond)
+
+    except subprocess.CalledProcessError as error:
+        raise error
+
+def trade_reversal_test():
+    entries = 2
+    md_etry_type1 = 5
+    md_price1 = 100000000000000
+
+    md_etry_type2 = 10
+    md_price2 = 255555555555555
+
+    pkt = IP()/UDP(sport=65333, dport=65333)/PacketHeader() / \
+        TradeReversal(NoMDEntries=entries)
+    pkt[PacketHeader].MessageHeader = MessageHeaderComp(TemplateID=13005)
+    pkt[TradeReversal].MessageHeader = MessageHeaderComp(TemplateID=13200)
+    pkt[TradeReversal].MDTradeEntryGrp = [MDTradeEntryGrpComp(MDEntryType=md_etry_type1, MDEntryPx=md_price1),
+                                            MDTradeEntryGrpComp(MDEntryType=md_etry_type2, MDEntryPx=md_price2)]
+
+    wrpcap('./test.pcap', pkt, append=False)
+    try:
+        result = subprocess.run(
+            ['tshark', '-V', '-r', 'test.pcap'], check=True, capture_output=True, text=True)
+        if print_if_error(result.stdout):
+            return
+        result = subprocess.run(
+            "tshark -T pdml -r test.pcap".split(" "), check=True, capture_output=True, text=True)
+        xml = ET.fromstring(result.stdout)
+        xml = xml.find(xml_path('message'))
+        # print(xml.attrib)
+        print('Testing for Trade Reversal Message Fields:')
+        print_if_not_match('NoMDEntries', get_show(xml, 'nomdentries'), entries)
+        print_if_not_match('MDEntryType 1', get_show(xml, 'mdentrytype'), md_etry_type1)
+        print_if_not_match('MDEntryPx 1', get_show(xml, 'mdentrypx'), md_price1 / pow(10, 8))
+
+        xml = xml.findall(xml_path('mdtradeentrygrpcomp'))[1]
+        print_if_not_match('MDEntryType 2', get_show(xml, 'mdentrytype'), md_etry_type2)
+        print_if_not_match('MDEntryPx 2', get_show(xml, 'mdentrypx'), md_price2 / pow(10, 8))
+    except subprocess.CalledProcessError as error:
+        raise error
+
+def order_add_test():
+    disply_qty = 244330000
+    price = 3123100000000
+    pkt = IP()/UDP(sport=65333, dport=65333)/PacketHeader()/OrderAdd()
+    pkt[PacketHeader].MessageHeader = MessageHeaderComp(TemplateID=13005)
+    pkt[OrderAdd].MessageHeader = MessageHeaderComp(TemplateID=13100)
+    pkt[OrderAdd].OrderDetails = OrderDetailsComp(DisplayQty=disply_qty, Price=price)
+
+    wrpcap('./test.pcap', pkt, append=False)
+    try:
+        result = subprocess.run(
+            ['tshark', '-V', '-r', 'test.pcap'], check=True, capture_output=True, text=True)
+        if print_if_error(result.stdout):
+            return
+        result = subprocess.run(
+            "tshark -T pdml -r test.pcap".split(" "), check=True, capture_output=True, text=True)
+        xml = ET.fromstring(result.stdout)
+        xml = xml.find(xml_path('message'))
+
+        print('Testing for Order Add Message Fields:')
+        print_if_not_match('DisplayQty', get_show(
+            xml, 'displayqty'), disply_qty / pow(10, 4))
+        print_if_not_match('Price', get_show(
+            xml, 'price'), price / pow(10, 8))
+    except subprocess.CalledProcessError as error:
+        raise error
+
+def top_of_book_test():
+    bid_px = 12300000000
+    offer_px = 33200000000
+    bid_size = 123400000
+    offer_size = 114320000
+
+    pkt = IP()/UDP(sport=65333, dport=65333)/PacketHeader()/TopOfBook(BidPx=bid_px, OfferPx=offer_px, BidSize=bid_size, OfferSize=offer_size)
+    pkt[PacketHeader].MessageHeader = MessageHeaderComp(TemplateID=13005)
+    pkt[TopOfBook].MessageHeader = MessageHeaderComp(TemplateID=13504)
+
+    wrpcap('./test.pcap', pkt, append=False)
+    try:
+        result = subprocess.run(
+            ['tshark', '-V', '-r', 'test.pcap'], check=True, capture_output=True, text=True)
+        if print_if_error(result.stdout):
+            return
+        result = subprocess.run(
+            "tshark -T pdml -r test.pcap".split(" "), check=True, capture_output=True, text=True)
+        xml = ET.fromstring(result.stdout)
+        xml = xml.find(xml_path('message'))
+
+        print('Testing for Top of Book Message Fields:')
+        print_if_not_match('BidPx', get_show(
+            xml, 'bidpx'), bid_px / pow(10, 8))
+        print_if_not_match('OfferPx', get_show(
+            xml, 'offerpx'), offer_px / pow(10, 8))
+        print_if_not_match('BidSize', get_show(
+            xml, 'bidsize'), bid_size / pow(10, 4))
+        print_if_not_match('OfferSize', get_show(
+            xml, 'offersize'), offer_size / pow(10, 4))
+    except subprocess.CalledProcessError as error:
+        raise error
+
+def auciton_best_test():
+    pste = 0
+    oot = 1
+    pkt = IP()/UDP(sport=65333, dport=65333)/PacketHeader()/AuctionBBO(PotentialSecurityTradingEvent=pste, OfferOrdType=oot)
+    pkt[PacketHeader].MessageHeader = MessageHeaderComp(TemplateID=13005)
+    pkt[AuctionBBO].MessageHeader = MessageHeaderComp(TemplateID=13500)
+
+    wrpcap('./test.pcap', pkt, append=False)
+    try:
+        result = subprocess.run(
+            ['tshark', '-V', '-r', 'test.pcap'], check=True, capture_output=True, text=True)
+        if print_if_error(result.stdout):
+            return
+        result = subprocess.run(
+            "tshark -T pdml -r test.pcap".split(" "), check=True, capture_output=True, text=True)
+        xml = ET.fromstring(result.stdout)
+        xml = xml.find(xml_path('message'))
+
+        print('Testing for Auction Best Message Fields:')
+        print_if_not_match('PotentialSecurityTradingEvent', get_show(
+            xml, 'potentialsecuritytradingevent'), pste)
+        print_if_not_match('OfferOrdType', get_show(
+            xml, 'offerordtype'), oot) 
+    except subprocess.CalledProcessError as error:
+        raise error
+
+def auction_clearing_test():
+    imb = 45230000
+    pste = 10
+    pkt = IP()/UDP(sport=65333, dport=65333)/PacketHeader() / \
+        AuctionClearingPrice(ImbalanceQty=imb, PotentialSecurityTradingEvent=pste)
+    pkt[PacketHeader].MessageHeader = MessageHeaderComp(TemplateID=13005)
+    pkt[AuctionClearingPrice].MessageHeader = MessageHeaderComp(TemplateID=13501)
+
+    wrpcap('./test.pcap', pkt, append=False)
+    try:
+        result = subprocess.run(
+            ['tshark', '-V', '-r', 'test.pcap'], check=True, capture_output=True, text=True)
+        if print_if_error(result.stdout):
+            return
+        result = subprocess.run(
+            "tshark -T pdml -r test.pcap".split(" "), check=True, capture_output=True, text=True)
+        xml = ET.fromstring(result.stdout)
+        xml = xml.find(xml_path('message'))
+
+        print('Testing for Auction Clearing Message Fields:')
+        print_if_not_match('PotentialSecurityTradingEvent', get_show(
+            xml, 'potentialsecuritytradingevent'), pste)
+        print_if_not_match('ImbalanceQty', get_show(
+            xml, 'imbalanceqty'), imb / pow(10, 4))
+    except subprocess.CalledProcessError as error:
+        raise error
+
+def product_state_change_test():
+    sid = 6
+    subid = 3
+    status = 2
+    indicator = 0
+    pkt = IP()/UDP(sport=65333, dport=65333)/PacketHeader() / \
+        ProductStateChange(TradingSessionID=sid, TradingSessionSubID=subid, TradSesStatus=status, FastMarketIndicator=indicator)
+    pkt[PacketHeader].MessageHeader = MessageHeaderComp(TemplateID=13005)
+    pkt[ProductStateChange].MessageHeader = MessageHeaderComp(
+        TemplateID=13300)
+
+    wrpcap('./test.pcap', pkt, append=False)
+    try:
+        result = subprocess.run(
+            ['tshark', '-V', '-r', 'test.pcap'], check=True, capture_output=True, text=True)
+        if print_if_error(result.stdout):
+            return
+        result = subprocess.run(
+            "tshark -T pdml -r test.pcap".split(" "), check=True, capture_output=True, text=True)
+        xml = ET.fromstring(result.stdout)
+        xml = xml.find(xml_path('message'))
+
+        print('Testing for Product State Change Message Fields:')
+        print_if_not_match('TradingSessionID', get_show(
+            xml, 'tradingsessionid'), sid)
+        print_if_not_match('TradingSessionSubID', get_show(
+            xml, 'tradingsessionsubid'), subid)
+        print_if_not_match('TradSesStatus', get_show(xml, 'tradsesstatus'), status)
+        print_if_not_match('FaseMarketIndicator', get_show(xml, 'fastmarketindicator'), indicator)
+    except subprocess.CalledProcessError as error:
+        raise error
+
+
+def instrument_summary_test():
+    update_time = 22313123151
+    no_orders = 33
+    sec_stat = 11
+    sec_tra_stat = 209
+    trd_event = 11
+    high_px = 2233100000000
+    mdentries = 1
+    entry_px = 12312300000000
+    entry_size = 7860000
+    entry_type = 66
+    trad_cond = 624
+
+    pkt = IP()/UDP(sport=65333, dport=65333)/PacketHeader() / \
+        InstrumentSummary(LastUpdateTime=update_time,
+                            TotNoOrders=no_orders,
+                            SecurityStatus=sec_stat,
+                            SecurityTradingStatus=sec_tra_stat,
+                            SecurityTradingEvent=trd_event,
+                            HighPx=high_px,
+                            NoMDEntries=mdentries)
+    pkt[PacketHeader].MessageHeader = MessageHeaderComp(TemplateID=13005)
+    pkt[InstrumentSummary].MessageHeader = MessageHeaderComp(
+        TemplateID=13601)
+    pkt[InstrumentSummary].MDInstrumentEntryGrp = [MDInstrumentEntryGrpComp(MDEntryPx=entry_px,
+                                                                                MDEntrySize=entry_size,
+                                                                                MDEntryType=entry_type,
+                                                                                TradeCondition=trad_cond)]
+
+    wrpcap('./test.pcap', pkt, append=False)
+    try:
+        result = subprocess.run(
+            ['tshark', '-V', '-r', 'test.pcap'], check=True, capture_output=True, text=True)
+        if print_if_error(result.stdout):
+            return
+        result = subprocess.run(
+            "tshark -T pdml -r test.pcap".split(" "), check=True, capture_output=True, text=True)
+        xml = ET.fromstring(result.stdout)
+        xml = xml.find(xml_path('message'))
+
+        print('Testing for Instrument Summary Message Fields:')
+        print_if_not_match('LastUpdateTime', get_show(
+            xml, 'lastupdatetime'), update_time)
+        print_if_not_match('TotNoOrders', get_show(
+            xml, 'totnoorders'), no_orders)
+        print_if_not_match('SecurityStatus', get_show(
+            xml, 'securitystatus'), sec_stat)
+        print_if_not_match('SecurityTradingEvent', get_show(
+            xml, 'securitytradingevent'), trd_event)
+        print_if_not_match('SecurityTradingStatus', get_show(
+            xml, 'securitytradingstatus'), sec_tra_stat)
+        print_if_not_match('HighPx', get_show(xml, 'highpx'), high_px / pow(10, 8))
+        print_if_not_match('NoMDEntries', get_show(xml, 'nomdentries'), mdentries)
+
+        print_if_not_match('MDEntryPx', get_show(xml, 'mdentrypx'), entry_px / pow(10, 8))
+        print_if_not_match('MDEntrySize', get_show(xml, 'mdentrysize'), entry_size / pow(10, 4))
+        print_if_not_match('MDEntryType', get_show(xml, 'mdentrytype'), entry_type)
+        print_if_not_match('TradeCondition', get_show(xml, 'tradecondition'), trad_cond)
+    except subprocess.CalledProcessError as error:
+        raise error
 
 def expected_to_fail():
     pkt = IP()/UDP(sport=65333, dport=65333)/PacketHeader()/AddComplexInstrument(NoLegs=4)
@@ -168,5 +474,15 @@ if __name__ == '__main__':
     packet_header_test()
     heart_beat_test()
     execution_summary_test()
+    quote_request_test()
+    cross_request_test()
+    trade_report_test()
+    trade_reversal_test()
+    order_add_test()
+    top_of_book_test()
+    auciton_best_test()
+    auction_clearing_test()
+    product_state_change_test()
+    instrument_summary_test()
     # add_compliex_instrument_test()
 
